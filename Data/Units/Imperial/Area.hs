61db7563eb39e2c5f3ebb7a661f96e3e9eb60ba1
ARJANEN Loïc Jean David <arjanen.loic@gmail.com>
2018-03-28 17:15:19 +0700

    Correction of some mistakes
    Adding a link to the UK legislations used as references, correction for some compile errors and some errors in the documentation comments.

--- Data/Units/Imperial/Area.hs
+++ Data/Units/Imperial/Area.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell, TypeFamilies, TypeOperators #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Units.Imperial.Area
@@ -13,15 +15,22 @@
 -- Where possible, reference have been made to UK legislation. However,
 -- Wikipedia's page is /much/ better organized than any government
 -- resource immediately available.
+--
+-- Where possible, reference have been made to UK legislation. However,
+-- Wikipedia's page is /much/ better organized than any government
+-- resource immediately available.
+-- The UK legislation used as references are as follows:
+-- <http://www.legislation.gov.uk/ukpga/1985/72/enacted>
+-- <http://www.legislation.gov.uk/uksi/1994/2867/schedule/part/VI/made>
+-- <http://www.legislation.gov.uk/uksi/1995/1804/schedule/made>
 -----------------------------------------------------------------------------
 
 module Data.Units.Imperial.Area where
 
+import Data.Metrology
 import Data.Metrology.TH
-
 import Data.Units.Imperial.Length
 import Data.Units.Imperial.Survey
-
 import Language.Haskell.TH
 
 declareDerivedUnit "Perch" [t| Rod :* Rod       |] 1 (Just "perch")
--- Data/Units/Imperial/Survey.hs
+++ Data/Units/Imperial/Survey.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell, TypeFamilies, TypeOperators #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Data.Units.Imperial.Survey
@@ -14,18 +16,24 @@
 -- Where possible, reference have been made to UK legislation. However,
 -- Wikipedia's page is /much/ better organized than any government
 -- resource immediately available.
+--
+-- Where possible, reference have been made to UK legislation. However,
+-- Wikipedia's page is /much/ better organized than any government
+-- resource immediately available.
+-- The UK legislation used as references are as follows:
+-- <http://www.legislation.gov.uk/ukpga/1985/72/enacted>
+-- <http://www.legislation.gov.uk/uksi/1994/2867/schedule/part/VI/made>
+-- <http://www.legislation.gov.uk/uksi/1995/1804/schedule/made>
 -----------------------------------------------------------------------------
 
 module Data.Units.Imperial.Survey where
 
 import Data.Metrology.TH
-
 import Data.Units.Imperial.Length
-
 import Language.Haskell.TH
 
-declareDerivedUnit "Link" [t| Rod   |] 0.04 (Just "li")
 declareDerivedUnit "Rod"  [t| Chain |] 0.25 (Just "rd")
+declareDerivedUnit "Link" [t| Rod   |] 0.04 (Just "li")
 
 -- | Survey lengths: All units above
 surveyLengths :: [Name]
